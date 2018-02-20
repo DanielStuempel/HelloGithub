6fb61ef59c0090a7c18c7e306dae182078eab77a
Daniel <Daniel@Notebook>
2018-02-20 21:13:42 +0100

    123

--- HelloGithub/src/com/kubus/hello/HelloWorld.java
+++ HelloGithub/src/com/kubus/hello/HelloWorld.java
@@ -3,7 +3,6 @@ package com.kubus.hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello, world");
-		System.out.println("test1");
 		System.exit(0);
 	}
 }
